dd7d5ccd3e7d6bceeb3f86c7c951cebdc965d39b
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-11-08 20:40:50 +0100

    Update script.js

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,7 +1,7 @@
 // 1. Change the title of the page to `Hello AltCampus!`
-
+document.title = Hello Altcampus
 // 2. Select the element using the children property:
-
+document.querySelector(`h1)
 //    - Select the `h1` element and change the value to `Learning DOM`
 
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
