5b68386fcc0d3d2557b5fe2ddb4431dd654e90f1
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-11-09 14:19:10 +0100

    Update script.js

---
+++ block-BJaaef/code/script.js
@@ -0,0 +1,18 @@
+let got = {
+  houses: [
+    {
+      name: "Starks",
+      wikiLink: "http://gameofthrones.wikia.com/wiki/House_Stark",
+      people: [
+        {
+          name: 'Eddard "Ned" Stark',
+          description:
+            "Lord of Winterfell - Warden of the North - Hand of the King - Married to Catelyn (Tully) Stark",
+          image:
+            "https://raw.githubusercontent.com/nnnkit/json-data-collections/master/game-of-thrones/game-of-thrones-eddard-stark.jpg",
+          wikiLink: "http://gameofthrones.wikia.com/wiki/Eddard_Stark",
+        },
+        
+        
+        
+        
